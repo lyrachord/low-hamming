1fee89a15798f58f2ca967ef7264c2491beb5fd2
Francisco <fvialp@gmail.com>
2018-09-03 11:19:53 -0300

    approved

--- C/parameters.hpp
+++ C/parameters.hpp
@@ -1,15 +1,15 @@
 
 // Number of bits of each factor
-#define BITS 512
+#define BITS 1024
 
 // One bit "1" in each PROB bits
-#define PROB 26
+#define PROB 50
 
 // Block size
-#define K 26
+#define K 70
 
 // Max weight per block
-#define MAX_WEIGHT 2
+#define MAX_WEIGHT 3
 
 // How many candidates to test per list
-#define SCOPE 3
+#define SCOPE 6
