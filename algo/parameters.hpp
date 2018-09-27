b473ccde776ee6d0f8be1271ca3b91f3cf522bbf
Francisco <fvialp@gmail.com>
2018-09-27 12:51:30 -0300

    adding biblio

--- algo/parameters.hpp
+++ algo/parameters.hpp
@@ -1,17 +1,17 @@
 // Number of bits of each factor
-#define BITS 2048
+#define BITS 1024
 
 // Poisson: One bit "1" in each PROB bits
 #define PROB 60
 
 // Random indices
 #define SET_BITS 20
-// Block size
 
-#define K 200
+// Block size
+#define K 100
 
 // Max weight per block
 #define MAX_WEIGHT 3
 
 // How many candidates to test per list
-#define SCOPE 100
+#define SCOPE 20
